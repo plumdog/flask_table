470efb9c5745b57fcbe90df74c5bbfe516ae24e6
Andrew Plummer <plummer574@gmail.com>
2014-05-03 19:07:08 +0100

    Python packaging - setup.py and README

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-Table',
+    packages=['flask_table'],
+    version='0.1.0',
+    author='Andrew Plummer',
+    author_email='plummer574@gmail.com',
+    url='https://github.com/plumdog/flask_table',
+    description='HTML tables for use with the Flask micro-framework',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Framework :: Flask',
+    ])
