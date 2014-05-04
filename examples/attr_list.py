c69a3d09f258c2fc8a4cf22151e051b27cc64c70
Andrew Plummer <plummer574@gmail.com>
2014-05-04 01:15:35 +0100

    Add basic examples

---
+++ examples/attr_list.py
@@ -0,0 +1,31 @@
+from flask_table import Table, Col
+
+
+"""Lets suppose that we have a class that we get an iterable of from
+somewhere, such as a database. We can declare a table that pulls out
+the relevant entries, escapes them and displays them.
+
+"""
+class Item(object):
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+class Category(object):
+    def __init__(self, name):
+        self.name = name
+
+class ItemTable(Table):
+    name = Col('Name')
+    category_name = Col('Category', attr_list=['category', 'name'])
+
+
+def main():
+    items = [Item('A', Category('catA')),
+             Item('B', Category('catB'))]
+
+    tab = ItemTable(items)
+    print(tab.__html__())
+
+if __name__ == '__main__':
+    main()
---
+++ examples/simple.py
@@ -0,0 +1,52 @@
+from flask_table import Table, Col
+
+
+"""Lets suppose that we have a class that we get an iterable of from
+somewhere, such as a database. We can declare a table that pulls out
+the relevant entries, escapes them and displays them.
+
+"""
+class Item(object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+class ItemTable(Table):
+    name = Col('Name')
+    description = Col('Description')
+
+
+def main():
+    items = [Item('A', 'aaa'),
+             Item('B', 'bbb')]
+
+    tab = ItemTable(items)
+
+    # or {{ tab }} in jinja
+    print(tab.__html__())
+
+    """Outputs:
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th></tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>A</td>
+          <td>aaa</td>
+        </tr>
+        <tr>
+          <td>B</td>
+          <td>bbb</td>
+        </tr>
+      </tbody>
+    </table>
+
+    Except it doesn't bother to prettify the output.
+    """
+
+if __name__ == '__main__':
+    main()
