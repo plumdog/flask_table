b4c5eff3273c4ac34cf79189218c62da4e2397d9
Andrew Plummer <plummer574@gmail.com>
2014-05-05 17:46:54 +0100

    Simplify imports for running tests

--- runtests.py
+++ runtests.py
@@ -1,11 +1,4 @@
 import unittest
-from tests import (TableTest, ColTest, AttrListTest, LinkTest, ButtonTest,
-                   EscapeTest, ColDictTest, AttrListDotsTest, ColCallableTest,
-                   AltAttrTest, ClassTest, LinkDictTest,
-                   LinkTestSubItemAttrList, LinkTestSubItemAttrDots,
-                   WeirdAttrListTest, BoolTest, OptTest, OptTestDefaultKey,
-                   OptTestDefaultValue, EmptyTest)
 
 if __name__ == '__main__':
-    #unittest.main(warnings=False)
-    unittest.main()
+    unittest.main(module='tests')
